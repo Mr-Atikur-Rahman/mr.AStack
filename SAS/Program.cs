fdcfe4051c04454f49c95ffaa308d8c2e10ff52f
Md. Atikur Rahman <batik.bu.cse@gmail.com>
2023-08-23 03:48:03 +0600

    ConsoleDone

--- SAS/Program.cs
+++ SAS/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("please enter login type");
+﻿using System.Net.NetworkInformation;
+
+Console.WriteLine("please enter login type");
 Console.WriteLine("Enter 1 for admin, 2 for teacher, 3 for student");
 string userChoice = Console.ReadLine();
 
@@ -14,6 +16,7 @@ if (userChoice == "1")
     Console.WriteLine("Enter Password");
     string inputpassword = Console.ReadLine();
     // inputUserId ebong inputpassword kono database er user tbl er mile kina!
+
     if (inputUserId == adminUserName && inputpassword == adminPassword)
     {
         Console.WriteLine("Welcome Admin");
@@ -138,59 +141,163 @@ if (userChoice == "1")
 
         else
         {
-            Console.WriteLine("Login unsuccessfull");
+            Console.WriteLine("Unknown Activity");
         }
+
     }
-    
-    else if (userChoice == "2")
+    else
     {
-        string TUserName = "teacher";
-        string TPassword = "1234";
-        Console.WriteLine("Please login");
-        Console.WriteLine("Enter Username");
-        string TUserId = Console.ReadLine();
+        Console.WriteLine("Choice Unknown");
+    }
+}
+else if (userChoice == "2")
+{
+    string TUserName = "teacher";
+    string TPassword = "1234";
+    Console.WriteLine("Please login");
+    Console.WriteLine("Enter Username");
+    string TUserId = Console.ReadLine();
 
-        Console.WriteLine("Enter Password");
-        string TInputpassword = Console.ReadLine();
+    Console.WriteLine("Enter Password");
+    string TInputpassword = Console.ReadLine();
 
-        if (TUserId == TUserName && TInputpassword == TPassword)
+    if (TUserId == TUserName && TInputpassword == TPassword)
+    {
+        Console.WriteLine("Welcome Teacher");
+        Console.WriteLine("Please enter your choice");
+        Console.WriteLine("1 for create student");
+        Console.WriteLine("2 for report: attendance check");
+        Console.WriteLine("3 for assign student in a course");
+        Console.WriteLine("4 for set Exam schedule for a course");
+
+        string TeacherActivity = Console.ReadLine();
+        if (TeacherActivity == "1")
         {
-            Console.WriteLine("Welcome Teacher");
-            Console.WriteLine("Please enter your choice");
-            Console.WriteLine("1 for create student");
-            Console.WriteLine("2 for report: attendance check");
-            Console.WriteLine("3 for assign student in a course");
-            Console.WriteLine("4 for set class schedule for a course");
-
-            string Activity = Console.ReadLine();
-
+            Console.WriteLine("===== Add a new Student ====");
+            //add teacher 
+            Console.WriteLine("Enter Student Name");
+            string StdName = Console.ReadLine();
+            Console.WriteLine("Enter Student Id");
+            string StdId = Console.ReadLine();
+            Console.WriteLine("Enter Student Activity");
+            string StdStatus = Console.ReadLine();
+            Console.WriteLine("Teacher Added with " + StdName + " and with salary of " + StdId + "Std Status" + StdStatus);
         }
-        else if (userChoice == "3")
+        else if (TeacherActivity == "2")
         {
-            string SUserName = "student";
-            string SPassword = "12345";
-            Console.WriteLine("Please login");
-            Console.WriteLine("Enter Username");
-            string SUserId = Console.ReadLine();
-
-            Console.WriteLine("Enter Password");
-            string SInputpassword = Console.ReadLine();
-
-            if (TUserId == SUserName && TInputpassword == SPassword)
-            {
-                Console.WriteLine("Welcome Student");
-                Console.WriteLine("Please enter your choice");
-                Console.WriteLine("1 for Give Attendance");
-                Console.WriteLine("2 for doing Exam");
-
-                string Activity = Console.ReadLine();
-
-            }
-
+            Console.WriteLine("===== Add an Attendance report ====");
+            //add teacher 
+            Console.WriteLine("Enter Total Student");
+            string CountStd = Console.ReadLine();
+            Console.WriteLine("Enter Total Present Student");
+            string CountPStd = Console.ReadLine();
+            Console.WriteLine("Enter Total Absent Student");
+            string CountAStd = Console.ReadLine();
+            Console.WriteLine("Report of Attendance " + CountStd + " Present of " + CountPStd + "Absent of" + CountAStd);
+        }
+        else if (TeacherActivity == "3")
+        {
+            Console.WriteLine("===== Assigning Students  ====");
+            //add teacher 
+            Console.WriteLine("Enter Student Name");
+            string StdName = Console.ReadLine();
+            Console.WriteLine("Enter Student Id");
+            string StdId = Console.ReadLine();
+            Console.WriteLine("Assigning Course Id");
+            string CourseId = Console.ReadLine();
+            Console.WriteLine("Report of Attendance " + StdName + " Present of " + StdId + "Absent of" + CourseId);
+        }
+        else if (TeacherActivity == "4")
+        {
+            Console.WriteLine("===== Exam Schedule  ====");
+            //add teacher 
+            Console.WriteLine("Enter Course Name");
+            string CId = Console.ReadLine();
+            Console.WriteLine("Enter Mid Term Schedule");
+            string XmMid = Console.ReadLine();
+            Console.WriteLine("Enter Final Term Schedule");
+            string XmFinal = Console.ReadLine();
+            Console.WriteLine("Exam Schedule " + CId + " Mid Term will take " + XmMid + "Final Term Will Take" + XmFinal);
         }
         else
         {
-            Console.WriteLine("Choice unknow");
+            Console.WriteLine("Unknown Activity");
         }
     }
+    else
+    {
+        Console.WriteLine("Choice Unknown");
+    }
 }
+else if (userChoice == "3")
+{
+    string SUserName = "student";
+    string SPassword = "12345";
+    Console.WriteLine("Please login");
+    Console.WriteLine("Enter Username");
+    string SUserId = Console.ReadLine();
+
+    Console.WriteLine("Enter Password");
+    string SInputpassword = Console.ReadLine();
+
+    if (SUserId == SUserName && SInputpassword == SPassword)
+    {
+        Console.WriteLine("Welcome Student");
+        Console.WriteLine("Please enter your choice");
+        Console.WriteLine("1 for Give Attendance");
+        Console.WriteLine("2 for doing Exam");
+        Console.WriteLine("3 for Extra Curriculum Activity");
+
+        string StudentActivity = Console.ReadLine();
+
+        if (StudentActivity == "1")
+        {
+            Console.WriteLine("===== Give Attendance ====");
+            //add teacher 
+            Console.WriteLine("Enter Student Name");
+            string StdName = Console.ReadLine();
+            Console.WriteLine("Present Plz");
+            string StdActivity = Console.ReadLine();
+            Console.WriteLine("Present Status");
+            string SPstatus = Console.ReadLine();
+
+            Console.WriteLine("Attendance Query " + StdName + " Presnt  " + StdActivity + " Yes/No " + SPstatus);
+
+        }
+        else if (StudentActivity == "2")
+        {
+
+            Console.WriteLine("===== Doing Exam ====");
+            //add teacher 
+            Console.WriteLine("Enter Course Name");
+            string CName = Console.ReadLine();
+            Console.WriteLine("Exam Type");
+            string XmType = Console.ReadLine(); // CT, MId, Final
+            Console.WriteLine("Attendance");
+            string Pstatus = Console.ReadLine(); // Yes or No
+
+            Console.WriteLine("Exam Query " + CName + " Presnt  " + XmType + " Yes/No " + Pstatus);
+        }
+
+        else if (StudentActivity == "3")
+        {
+
+            Console.WriteLine("===== Student Extra Curriculum Activity ====");
+            //add teacher 
+            Console.WriteLine("Enter Extra Curriculum Activity");
+            string EName = Console.ReadLine();
+            Console.WriteLine("Extra Curriculum ActivityType");
+            string EType = Console.ReadLine(); // Expert, Moderate
+
+            Console.WriteLine("Extra Curriculum Activity Query " + EName + " Type or Position  " + EType);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Choice Unknown");
+    }
+}
+else
+    {
+        Console.WriteLine("Login unsuccessfull");
+    }
